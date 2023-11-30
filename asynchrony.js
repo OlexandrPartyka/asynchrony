4f30a12c2a380b3779b1bd50948ec5c2343af72b
OlexandrPartyka <144223043+OlexandrPartyka@users.noreply.github.com>
2023-11-30 20:35:32 +0200

    Update asynchrony.js

--- asynchrony.js
+++ asynchrony.js
@@ -1,1 +1,17 @@
+const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function delayedFunction() {
+  console.log("Ця функція виконується після затримки.");
+}
+
+rl.question("Введіть час затримки в секундах: ", (delayTime) => {
+    delayTime = delayTime * 1000
+  setTimeout(delayedFunction, delayTime);
+
+  rl.close(); 
+});
