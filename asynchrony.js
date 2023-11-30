8ba82521bc661efb44dddcb7ea4b5d144e005b40
OlexandrPartyka <144223043+OlexandrPartyka@users.noreply.github.com>
2023-11-30 20:32:55 +0200

    Update asynchrony.js

--- asynchrony.js
+++ asynchrony.js
@@ -1,6 +1,1 @@
-function delayedFunction() {
-  console.log("Ця функція виконується після затримки.");
-}
-var delayTime = prompt("Введіть час затримки в мілісекундах:");
 
-setTimeout(delayedFunction, delayTime);
